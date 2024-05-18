490d6e874bc43cbb49220e70ed8bcd86155a56b4
K.K <gokennsirou@yahoo.co.jp>
2024-05-18 21:36:41 +0900

    記事CRUDのAPI Routeを削除

--- src/features/articles/models/article.ts
+++ src/features/articles/models/article.ts
@@ -6,20 +6,6 @@ export { Article };
 
 export const ARTICLE_LIST_SORTS = ["createdAt:asc", "createdAt:desc"] as const;
 
-/** APIを通すとDateでなくなるので、Dateに戻す */
-export const convertToArticle = (
-  article: Omit<Article, "createdAt" | "updatedAt"> & {
-    createdAt: string;
-    updatedAt: string;
-  },
-) => {
-  return {
-    ...article,
-    createdAt: new Date(article.createdAt),
-    updatedAt: new Date(article.updatedAt),
-  };
-};
-
 export const MIN_TITLE_LENGTH = 1;
 export const MAX_TITLE_LENGTH = 255;
 export const MIN_CONTENT_LENGTH = 1;
