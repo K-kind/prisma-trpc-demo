879a0b14543a0eb8069ad7a34aa145dc442dcbce
K.K <gokennsirou@yahoo.co.jp>
2024-05-26 20:05:36 +0900

    Buttonにloading propsを追加

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,11 +1,12 @@
-import { Slot } from "@radix-ui/react-slot";
+import { Slot, Slottable } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
+import { Loader2 } from "lucide-react";
 import * as React from "react";
 
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none relative",
   {
     variants: {
       variant: {
@@ -25,6 +26,12 @@ const buttonVariants = cva(
         lg: "h-11 rounded-md px-8",
         icon: "h-10 w-10",
       },
+      disabled: {
+        true: "opacity-50 pointer-events-none",
+      },
+      loading: {
+        true: "",
+      },
     },
     defaultVariants: {
       variant: "default",
@@ -35,19 +42,29 @@ const buttonVariants = cva(
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
+    Omit<VariantProps<typeof buttonVariants>, "disabled"> {
   asChild?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  (
+    { className, variant, size, loading, children, asChild = false, ...props },
+    ref,
+  ) => {
     const Comp = asChild ? Slot : "button";
+    const disabled = (props.disabled || loading) ?? undefined;
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
+        className={cn(
+          buttonVariants({ variant, size, loading, disabled, className }),
+        )}
         ref={ref}
+        disabled={disabled}
         {...props}
-      />
+      >
+        {loading && <Loader2 className="animate-spin absolute" />}
+        <Slottable>{children}</Slottable>
+      </Comp>
     );
   },
 );
